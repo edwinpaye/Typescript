b96a6f41f2442bd7b46766117f9e2e4fd4759827
edwinpaye <baratheonts@gmail.com>
2020-01-15 22:30:17 -0400

    add HttpClientModule

--- angular-practice-app/src/app/list/list.service.ts
+++ angular-practice-app/src/app/list/list.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { LIST } from './mocks.json';
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
 
-  constructor() { }
+  private urlEndpoint: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
 
   getList(): Observable<any[]>{
-    return of(LIST);
+    // return of(LIST);
+    return this.http.get<any[]>(this.urlEndpoint).pipe(
+      map( response => response as any[] )
+    );
   } 
 
 }
